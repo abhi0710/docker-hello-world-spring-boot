278d3da79f53aad322b8cc49a7fed4b281240fea
Abhinav Agarwal <abhi0710@gmail.com>
2020-05-01 12:42:46 +0530

    rest check

--- src/main/java/com/dockerforjavadevelopers/hello/Application.java
+++ src/main/java/com/dockerforjavadevelopers/hello/Application.java
@@ -4,8 +4,10 @@ package com.dockerforjavadevelopers.hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableAutoConfiguration
@@ -15,6 +17,12 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
         
+    }
+
+
+    @Bean
+    public RestTemplate template(){
+        return new RestTemplate();
     }
 
 }
--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -1,15 +1,27 @@
 package com.dockerforjavadevelopers.hello;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HelloController {
-    
+
+    Logger logger = LoggerFactory.getLogger(HelloController.class.getName());
+
+    @Autowired
+    private RestTemplate template;
+
     @RequestMapping("/")
     public String index() {
+        System.out.println("In ");
+        System.out.flush();
+        System.out.println(template.getForEntity("https://www.google.com/", String.class).getBody());
         return "Hello World\n";
     }
-    
+
 }
